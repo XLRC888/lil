23dbe6f73072e38bbfa47b31abbe91591c5e5013
XLRC888 <181596240+XLRC888@users.noreply.github.com>
2026-06-28 06:31:50 +0300

    deploy

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bf3ff565.js',
+                path_to_root + 'searchindex-603db682.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
