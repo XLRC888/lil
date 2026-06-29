3554c157ee69b0bfc3b694bdbbc79721e6b334c7
XLRC888 <XLRC888@users.noreply.github.com>
2026-06-29 22:05:18 +0000

    deploy: be8f77c284575f979b7ce2a3a1c0c4154b65a0b7

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4480de0f.js',
+                path_to_root + 'searchindex-da51dd9f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
